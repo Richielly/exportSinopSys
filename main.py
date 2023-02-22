eb64d5257a3b6d0f0886721408686d01dae4e204
Richielly <richielly.positivo@gmail.com>
2023-02-22 08:11:14 -0300

    v_0.2

--- main.py
+++ main.py
@@ -2,59 +2,110 @@ import time
 import fdb
 import configparser
 import script
-import PySimpleGUI as sg
-from random import randrange as r
+from bottle import get, post, route, run, debug, template, request, static_file, error
+
 
 cfg = configparser.ConfigParser()
 cfg.read('cfg.ini')
 
 # dados_conexao = fdb.connect(host='localhost', database=r'D:\Unifica\CM_Senges\origem\EQUIPLANO.FDB', user='sysdba', port=3050, password='masterkey')
-dados_conexao = fdb.connect(host='localhost', database=cfg['DEFAULT']['NomeBanco'], user='sysdba', port=3050, password='masterkey')
-cur = dados_conexao.cursor()
 
-comandos = script.scripts
+@route('/')
 def start():
-    tamanho = 0
-    event, values = window.read()
+    dados_conexao = fdb.connect(host='localhost', database=cfg['DEFAULT']['NomeBanco'], user='sysdba', port=3050,
+                                password='masterkey')
+    cur = dados_conexao.cursor()
+
+    dados_conexao_2 = fdb.connect(host='localhost', database=cfg['DEFAULT']['NomeBanco_2'], user='sysdba', port=3050,
+                                password='masterkey')
+    cur_2 = dados_conexao_2.cursor()
+
+    comandos = script.scripts
     while True:
-        if event == 'Iniciar':
-            if event == 'Cancel' or event == sg.WIN_CLOSED:
-                break
-            for comando in comandos:
-                tamanho += 1
-                print('Arquivo: ' + comando + ' iniciado em ' + time.strftime("%d/%m/%y %H:%M:%S"))
-                cur.execute(comandos[comando])
-                result = cur.fetchall()
+        for comando in comandos:
+            print('Arquivo: ' + comando + ' iniciado em ' + time.strftime("%d/%m/%y %H:%M:%S"))
+            cur.execute(comandos[comando])
+            result = cur.fetchall()
 
-                arquivo = open(cfg['DEFAULT']['DiretorioArquivos'] + comando + '_' + cfg['DEFAULT']['CodEntidade'] + '.txt', "w", newline='', encoding='ANSI')
-                arquivo2 = open(cfg['DEFAULT']['DiretorioArquivos'] + comando + '_' + cfg['DEFAULT']['CodEntidade'] + '_teste.txt', "w", newline='', encoding='ANSI')
+            cur.execute('select a.dados, a.dadosveiculo, a.sequencia from fromovc a where a.TIPO = 6')
+            result_1 = cur.fetchall()
 
+            arquivo = open(cfg['DEFAULT']['DiretorioArquivos'] + comando + '_' + cfg['DEFAULT']['CodEntidade'] + '.txt', "w", newline='', encoding='ANSI')
+            arquivo2 = open(cfg['DEFAULT']['DiretorioArquivos'] + comando + '_' + cfg['DEFAULT']['CodEntidade'] + '_teste.txt', "w", newline='', encoding='ANSI')
 
-                arquivo2.write(str(result[0]).split('|')[6].replace(':', '|') + '|')  # codigoproduto|nome
-                arquivo2.write(str(result[1][1].split('|')[0])) # codigomotorista
+            # coluna dados --> posição 1= num nota fiscal      2 = km atual    3 = qtde litros    4 = valor    5 = cod fornecedor     6 = razão social fornecedor   7 = tipo combustivel
+            #  arquivo2.write(str(result[0]).split('|')[6].replace(':', '|') + '|')  # codigoproduto|nome
+            # arquivo2.write(str(result[1][1].split('|')[0])+'|') # codigomotorista
 
-                for inf in result:
-                    arquivo.write(str(inf[0])+'\n')
-                print('Arquivo: ' + comando + ' finalizado em ' + time.strftime("%d/%m/%y %H:%M:%S") +'\n')
-                progress_bar.UpdateBar(tamanho + 1)
-                window['status'].update(str(tamanho) + '/' + str(len(comandos)))
+            # print(result[0][0]) #coluna dados
+            # print(result[0][1]) #coluna dados veiculo
 
-            sg.SystemTray.notify('Arquivos gerados com sucesso.', cfg['DEFAULT']['NomeEntidade'].replace("'", ''))
+            # select dados, dadosveiculo from fromovc
+
+            dado = str(result_1[0][0]).split('|')
+            dados_veiculo = str(result_1[0][1]).split('|')
+
+            CodProduto_Nome = dado[6].replace(':', '|')
+            NrCodigoMotorista = dados_veiculo[0]
+            VlUnitario = round(float(str(result_1[0][0]).split('|')[3].replace('.', '').replace(',', '.').replace('R$ ', '')) / float(str(result_1[0][0]).split('|')[2].replace('.', '').replace(',', '.').replace('R$ ', '')), 4)  # valor unitario
+            NrLitrosAbastecimento = dado[2].replace(',', '.')
+            VlAbastecimento = dado[3].replace(',', '.')
+            NrNotaFiscal = dado[0]
+            CodFornecedor = dado[4]
+            cur_2.execute(f""" select f.NUMCNPJPESQ from FINPESC f where codigo = {CodFornecedor}""")
+            result_2 = cur_2.fetchall()
+            Cnpj = result_2[0][0]
+
+            NrFrota = dados_veiculo[2]
+
+            for inf in result:
+                sequencia = str(inf[0]).split('|')[1]
+                cur.execute(f'select a.dados, a.dadosveiculo, a.sequencia from fromovc a where a.TIPO = 6 and a.sequencia = {sequencia}')
+                result_1 = cur.fetchall()
+
+                dado = str(result_1[0][0]).split('|')
+                dados_veiculo = str(result_1[0][1]).split('|')
+
+                CodProduto_Nome = dado[6].replace(':', '|')
+                NrCodigoMotorista = dados_veiculo[0]
+                VlUnitario = round(float(str(result_1[0][0]).split('|')[3].replace('.', '').replace(',', '.').replace('R$ ', '')) / float(str(result_1[0][0]).split('|')[2].replace('.', '').replace(',', '.').replace('R$ ', '')),4)  # valor unitario
+                NrLitrosAbastecimento = dado[2].replace(',', '.')
+                VlAbastecimento = dado[3].replace(',', '.')
+                NrNotaFiscal = dado[0]
+                CodFornecedor = dado[4]
+
+                try:
+                    if CodFornecedor == '0':
+                        CodFornecedor = '13'
+                        cur_2.execute(f""" select f.NUMCNPJPESQ from FINPESC f where codigo = {CodFornecedor}""")
+
+                    else:
+                        cur_2.execute(f""" select f.NUMCNPJPESQ from FINPESC f where codigo = {CodFornecedor}""")
+                    result_2 = cur_2.fetchall()
+                    Cnpj = result_2[0][0]
+
+                except: CodFornecedor = '13'
+
+                NrFrota = dados_veiculo[2]
+
+                print(NrFrota)
+
+                arquivo.write(str(inf[0]).replace('$CodProduto$|$Nome$', CodProduto_Nome).replace('$NrCodigoMotorista$', NrCodigoMotorista).replace('$VlUnitario$', str(VlUnitario)).replace('$NrLitrosAbastecimento$', NrLitrosAbastecimento).replace('$VlAbastecimento$',VlAbastecimento).replace('$NrNotaFiscal$',NrNotaFiscal).replace('$CodFornecedor$', CodFornecedor).replace('$Cnpj$',Cnpj).replace('.',',')+'\n')
+
+            print('Arquivo: ' + comando + ' finalizado em ' + time.strftime("%d/%m/%y %H:%M:%S") + '\n')
+
+        print('Arquivos gerados com sucesso.', cfg['DEFAULT']['NomeEntidade'].replace("'", ''))
 
         dados_conexao.close()
         cur.close()
-        time.sleep(2)
+        with open(cfg['DEFAULT']['DiretorioArquivos'] + comando + '_' + cfg['DEFAULT']['CodEntidade'] + '_teste.txt', "r", encoding='ANSI') as dados:
+            resp = dados.readlines()
+            print('teste', str(resp))
+            print(cfg['DEFAULT']['DiretorioArquivos'] + comando + '_' + cfg['DEFAULT']['CodEntidade'] + '_teste.txt')
         break
+    return template('template/index', mensagem='Arquivos gerados com sucesso para ' + cfg['DEFAULT']['NomeEntidade'].replace("'", '') +' \nCaminho '+ cfg['DEFAULT']['DiretorioArquivos'], dados=resp )
 
-layout = [[sg.Text('Gerando Arquivos')],
-          [sg.Text('0/'+str(len(comandos)), key='status')],
-          [sg.Output(size=(80,20))],
-          [sg.ProgressBar(len(comandos), orientation='h', size=(53, 20), key='progressbar')],
-          [sg.OK('Iniciar'), sg.Cancel()]]
 
-window = sg.Window('Arquivos - ' + cfg['DEFAULT']['CodEntidade'] + ' - ' + cfg['DEFAULT']['NomeEntidade'].replace("'", '') , layout)
-progress_bar = window['progressbar']
+run(port=6969, reloader=True)
 
-start()
-
-# pyinstaller --name export_unifica_frotas --onefile --noconsole main.py
+# pyinstaller --name export_sinopsys_frotas --onefile --noconsole main.py
--- script.py
+++ script.py
@@ -9,12 +9,40 @@ motorista = cfg['DEFAULT']['Motorista']
 
 scripts = {
 
-'Abastecimento' : f""" SELECT
-                    a.dados,
-                    a.dadosveiculo
-                    FROM FROMOVC a
-                    left join FROVEIC v on (v.IDFROTA = a.IDFROTA)
-                    join FROROTC r on (r.CODIGO = a.CODROTEIRO)
-                    where a.TIPO = 6
-                    order by a.SEQUENCIA desc """
+'Abastecimento' : f""" SELECT 
+{codEntidade} ||'|'||
+a.SEQUENCIA ||'|'||
+coalesce(v.CODIGO,'') ||'|'||
+coalesce(v.NUMTOMBAMENTO,'') ||'|'||
+'$CodProduto$' ||'|'||
+'$Nome$' ||'|'||
+'$NrCodigoMotorista$' ||'|'||
+'$VlUnitario$' ||'|'||
+substring(a.data from 9 for 2) ||'/'|| substring(a.data from 6 for 2) ||'/'|| substring(a.data from 1 for 4) ||'|'||
+'$NrLitrosAbastecimento$' ||'|'||
+'$VlAbastecimento$' ||'|'||
+'1' ||'|'||
+'$NrNotaFiscal$' ||'|'||
+coalesce(a.OBSERVACAO, '') ||'|'||
+'$CodFornecedor$' ||'|'||
+'$Cnpj$' ||'|'||
+'CodPessoa' ||'|'||
+'CodLocal' ||'|'||
+'NrInterno' ||'|'||
+1  ||'|'||
+'CodEntidadeLiquidacao' ||'|'||
+'ExercicioLiquidacao' ||'|'||
+'NrLiquidacao' ||'|'||
+'ExLiquidacao' ||'|'||
+'CodEntidadeOrigemLiquidacao' ||'|'||
+'CodEntidadeEmpenho' ||'|'||
+'ExercicioEmpenho' ||'|'||
+'NrEmpenho' ||'|'||
+'ExEmpenho' ||'|'||
+'CodEntidadeOrigemEmpenho' ||'|'
+FROM FROMOVC a 
+left join FROVEIC v on (v.IDFROTA = a.IDFROTA)
+join FROROTC r on (r.CODIGO = a.CODROTEIRO)
+where a.TIPO = 6
+order by a.SEQUENCIA desc """
 }
